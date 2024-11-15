8ae8b1fe85ea735960a1375a1938244fe5cc5f3d
Nagpal45 <nagpal4503@gmail.com>
2024-11-15 16:12:54 +0530

    cors added

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,18 @@ import { jwtVerify } from 'jose';
 
 export async function middleware(request: NextRequest) {
     const token = request.cookies.get('token');
+    const origin = request.headers.get('origin');
+    const allowedOrigins = ['https://car-management-phi.vercel.app', 'http://localhost:3000'];
 
+    if (request.method === 'OPTIONS') {
+        const response = new NextResponse(null, { status: 204 });
+        response.headers.set('Access-Control-Allow-Origin', allowedOrigins.includes(origin!) ? origin! : '');
+        response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        return response;
+    }
+
+    // Check for token
     if (!token) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
@@ -16,7 +27,7 @@ export async function middleware(request: NextRequest) {
         }
 
         const { payload } = await jwtVerify(token.value, new TextEncoder().encode(secret));
-        
+
         const response = NextResponse.next();
 
         if (payload && 'userId' in payload) {
@@ -24,6 +35,12 @@ export async function middleware(request: NextRequest) {
         } else {
             throw new Error('Invalid token payload');
         }
+        if (origin && allowedOrigins.includes(origin)) {
+            response.headers.set('Access-Control-Allow-Origin', origin);
+            response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+            response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        }
+
         return response;
     } catch (error) {
         console.log(error);
