85e5ba2a5d5082cbd9d6bde75aee065e9ebf9273
Nagpal45 <nagpal4503@gmail.com>
2024-11-15 13:29:27 +0530

    product detail page

--- src/app/productList/page.tsx
+++ src/app/productList/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import Image from 'next/image';
+import { useRouter } from "next/navigation";
 
 export default function ProductList() {
   interface Product {
@@ -13,6 +14,7 @@ export default function ProductList() {
 
   const [products, setProducts] = useState<Product[]>([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -38,15 +40,18 @@ export default function ProductList() {
         {products.map((product) => (
           <div key={product._id} className="mt-10 w-full h-[150px] flex flex-row border shadow-lg rounded-[20px] overflow-hidden">
             <Image src={product.images[0] || 'https://images.pexels.com/photos/29355951/pexels-photo-29355951/free-photo-of-audi-sedan-parked-in-urban-setting-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} width={600} height={100} alt="product image" className="w-[20%]" />
-            <div className="px-5 py-3 flex flex-col justify-between overflow-hidden">
+            <div className="px-5 py-3 flex flex-col justify-between overflow-hidden w-full">
               <div className="height-1/2 overflow-hidden">
                 <p className="font-bold text-[25px]">{product.title}</p>
                 {product.desc.length > 250 ? <p>{product.desc.slice(0, 250)}...</p> : <p>{product.desc}</p>}
               </div>
-              <div className="flex flex-row gap-5">
-                {product.tags.map((tag) => (
-                  <p key={tag} className="border border-black px-3 py-0.5 rounded-[30px] text-[14px]">{tag}</p>
-                ))}
+              <div className="flex flex-row justify-between w-full">
+                <div className="flex flex-row gap-5">
+                  {product.tags.map((tag) => (
+                    <p key={tag} className="border border-black px-3 py-0.5 rounded-[30px] text-[14px]">{tag}</p>
+                  ))}
+                </div>
+                <p className="border border-black px-3 rounded mb-1 cursor-pointer" onClick={() => router.push(`/product/${product._id}`)}>View</p>
               </div>
             </div>
           </div>
