f04543b8746608b792ffbbd08082a5ab3894a44c
Nagpal45 <nagpal4503@gmail.com>
2024-11-16 13:24:14 +0530

    home cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+"use client"
+import { useUser } from "@/lib/authContext";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const {isLoggedIn} = useUser()
   return (
     <div className="flex flex-row items-center justify-center h-screen">
       <div className="flex items-center justify-center h-screen w-1/2 overflow-hidden">
@@ -10,7 +13,7 @@ export default function Home() {
       <div className="flex flex-col items-start justify-center h-screen w-1/2 pl-[7%]">
         <h1 className="text-[80px] font-bold leading-[1.3] mt-10">Manage Your Cars Seamlessly</h1>
         <p className="text-[60px] w-[70%] mt-10 leading-[1.3]">All Things Car, All in One Place.</p>
-        <Link href='/productList' className="w-[50%] h-[60px] bg-blue-500 rounded-[30px] mt-14 font-bold text-white flex items-center justify-center"><button>Get Started</button></Link>
+        <Link href={isLoggedIn ? '/productList' : '/login'} className="w-[50%] h-[60px] bg-blue-500 rounded-[30px] mt-14 font-bold text-white flex items-center justify-center"><button>Get Started</button></Link>
       </div>
     </div>
   );
